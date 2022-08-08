2206bf3e369906d90be452d2b932b9cd75aa365b
Bertram Richter <bertram.richter@tu-dresden.de>
2022-08-08 15:32:33 +0200

    Updated documentation on shrinking.

--- fosanalysis/shrinking.py
+++ fosanalysis/shrinking.py
@@ -1,10 +1,9 @@
 
 ## \file
 ## Contains functionality to compensate shrinking and creep.
-## \todo Implement and document 
 ## \author Bertram Richter
 ## \date 2022
-## \package shrinking \copydoc shrinking.py
+## \package fosanalysis.shrinking \copydoc shrinking.py
 
 import numpy as np
 import scipy.signal
@@ -13,29 +12,39 @@ import fosutils
 
 class ShrinkCompensator():
 	def __init__(self,
-				strain_inst: np.array,
-				x_inst: np.array,
-				method: str,
-				*args, **kwargs):
+			strain_inst: np.array,
+			x_inst: np.array,
+			method: str = "mean_min",
+			*args, **kwargs):
 		"""
-		\todo Implement and document 
+		Constructs a ShrinkCompensator object.
+		\param strain_inst \copybrief strain_inst For more, see \ref strain_inst.
+		\param x_inst \copybrief x_inst For more, see \ref x_inst.
+		\param method \copybrief method For more, see \ref method.
+		\param args \copybrief args For more, see \ref args.
+		\param kwargs \copybrief kwargs For more, see \ref kwargs.
+		
+		\todo Apply sanitization to \ref strain_inst and \ref strain_inst.
 		"""
 		## Method, how to calculate the shrinkage calibration. Available options:
 		## - `"mean_min"`: (default) For all entries in local minima in `y_inst`, the difference to the same value in `y_inf` is measured.
 		## 	Afterwards the mean over the differences is taken.
 		self.method = method
-		## \todo Implement and document 
-		self.strain_inst
-		## \todo Implement and document 
+		## Instantaneous strain, that appear right after the applying load to the structure. 
+		self.strain_inst = strain_inst
+		## Positional data according to \ref strain_inst.
 		self.x_inst = x_inst
-		## \todo Implement and document 
+		## Positional arguments, will be passed to [`scipy.signal.find_peaks()`](https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html).
+		## By default empty.
 		self.args = args
-		## \todo Implement and document 
+		## Keyword arguments, will be passed to [`scipy.signal.find_peaks()`](https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html).
+		## By default empty.
 		self.kwargs = kwargs
-	def run(self, x, strain) -> np.array:
+	def run(self, x: np.array, strain: np.array) -> np.array:
 			"""
 			The influence of concrete creep and shrinking is calculated.
-			\todo Fix stripping
+			\param x Positional data.
+			\param strain Strain data.
 			"""
 			peaks_min, properties = scipy.signal.find_peaks(-self.strain_inst, *self.args, **self.kwargs)
 			# Get x positions and y-values for instantanious deformation
