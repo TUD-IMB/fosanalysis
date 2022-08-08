82ee04523c3d1932d93215a7828fa1f403cd8c4a
Bertram Richter <bertram.richter@tu-dresden.de>
2022-08-08 12:39:16 +0200

    Updated documentation on filtering.

--- fosanalysis/filtering.py
+++ fosanalysis/filtering.py
@@ -1,10 +1,9 @@
 
 ## \file
-## Contains filtering and sanitization, smoothing and healing functions.
-## \todo Implement and document
+## Contains filtering, sanitization, smoothing and healing functionalities.
 ## \author Bertram Richter
 ## \date 2022
-## \package filter \copydoc filter.py
+## \package fosanalysis.filtering \copydoc filtering.py
 
 from abc import ABC, abstractmethod
 import copy
@@ -12,46 +11,63 @@ import numpy as np
 
 class Filter(ABC):
 	"""
-	\todo Implement and document
+	Abstract base class for filter classes.
 	"""
 	def __init__(self,
 			*args, **kwargs):
 		"""
-		\todo Implement and document
+		Constructs a Filter object.
+		\param *args Additional positional arguments, will be passed to the superconstructor.
+		\param **kwargs Additional keyword arguments will be passed to the superconstructor.
 		"""
 		super().__init__(*args, **kwargs)
 	@abstractmethod
-	def run(self, data):
+	def run(self, data, *args, **kwargs):
 		"""
-		\todo Implement and document
+		Each Filter object has a `run()` method, which takes an iterable `data` as the first parameter and possibly additional positional and keyword arguments.
+		\param data Data, the will be filtered.
+		\param *args Additional positional arguments, to customize the behaviour.
+		\param **kwargs Additional keyword arguments to customize the behaviour.
 		"""
 		raise NotImplementedError()
 
 class Limit(Filter):
 	"""
-	\todo Implement and document
+	A filter to limit the entries.
+	The result \f$y\f$ will only contain all entries for which \f$y_i \in [x_{\mathrm{min}},\: x_{\mathrm{max}}]\f$ holds.
+	Values, that exceed the limits, will be truncated at the according limit using the equation
+	\f[y_i = \min\left(\max\left(x_i,\: x_{\mathrm{min}}\right),\: x_{\mathrm{max}}\right)\f].
 	"""
 	def __init__(self,
 			minimum: float = None,
 			maximum: float = None,
 			*args, **kwargs):
 		"""
-		\todo Implement and document
+		Constructs a Limit filter object.
+		\param minimum \copybrief minimum For more, see \ref minimum.
+		\param maximum \copybrief maximum For more, see \ref maximum.
+		\param *args Additional positional arguments, will be passed to the superconstructor.
+		\param **kwargs Additional keyword arguments will be passed to the superconstructor.
 		"""
 		super().__init__(*args, **kwargs)
-		## \todo Implement and document 
+		## Minimal value, which will be included in the result.
+		## All entries with values greater than that will be excluded.
 		self.minimum = minimum
-		## \todo Implement and document 
+		## Maximal value, which will be included in the result.
+		## All entries with values less than that will be excluded.
 		self.maximum = maximum
-	def run(self, data: np.array, minimum: float = None, maximum: float = None) -> np.array:
+	def run(self,
+			data: np.array,
+			minimum: float = None,
+			maximum: float = None,
+			) -> np.array:
 		"""
 		Limit the the entries in the given list to the specified range.
 		Returns a list, which conforms to \f$\mathrm{minimum} \leq x \leq \mathrm{maximum} \forall x \in X\f$.
 		Entries, which exceed the given range are cropped to it.
-		\param values List of floats, which are to be cropped.
+		\param data List of floats, which are to be cropped.
 		\param minimum Minimum value, for the entries. Defaults to `None`, no limit is applied.
 		\param maximum Maximum value, for the entries. Defaults to `None`, no limit is applied.
-		\todo Rename any remaining `limit_entry_values` to this function
 		"""
 		minimum = minimum if minimum is not None else self.minimum
 		maximum = maximum if maximum is not None else self.maximum
@@ -64,42 +80,58 @@ class Limit(Filter):
 
 class NaNFilter(Filter):
 	"""
-	\todo Implement and document
+	A filter, that removes any columns from a given number of data sets (matrix), tha contain `not a number` entries.
 	"""
 	def __init__(self,
 			*args, **kwargs):
 		"""
-		\todo Implement and document
+		Constructs a NaNFilter object.
+		\param *args Additional positional arguments, will be passed to the superconstructor.
+		\param **kwargs Additional keyword arguments will be passed to the superconstructor.
 		"""
 		super().__init__(*args, **kwargs)
-	def run(self, *value_lists) -> tuple:
+	def run(self,
+			*data_list,
+			exclude: list = None,
+			) -> tuple:
 		"""
-		In all given arrays, all entries are stripped, that contain `None`, `nan` or `""` in any of the given list.
-		\return Returns a tuple of with copies of the arrays. If only a single array is given, only the stripped copy returned.
-		\todo Rename any remaining `strip_nan_entries` to this function
+		In all given arrays of `data_list`, all entries are stripped, that contain `None`, `nan` or `""` in any of the given list.
+		\param data_list Tuple of arrays (matrix), which should be cleaned.
+		\param exclude Additional values that should be excluded. Defaults to nothing.
+		\return Returns a tuple with copies of the arrays, without columns containing any of the specified values. If only a single array is given, only the stripped copy returned.
 		"""
+		exclude = exclude if exclude is not None else []
+		exclude_set = set(None, "")
+		exclude_set.update(set(exclude))
 		stripped_lists = []
 		delete_list = []
 		# find all NaNs
-		for candidate_list in value_lists:
+		for candidate_list in data_list:
 			for i, entry in enumerate(candidate_list):
-				if entry is None or entry == "" or np.isnan(entry):
+				if entry in exclude_set or np.isnan(entry):
 					delete_list.append(i)
 		# strip the NaNs
-		for candidate_list in value_lists:
+		for candidate_list in data_list:
 			stripped_lists.append(np.array([entry for i, entry in enumerate(candidate_list) if i not in delete_list]))
 		return stripped_lists[0] if len(stripped_lists) == 1 else tuple(stripped_lists)
 
 class SlidingMean(Filter):
 	"""
-	\todo Implement and document
+	A filter, that smoothes the record using the mean over \f$2r + 1\f$ entries for each entry.
+	For each entry, the sliding mean extends \ref radius \f$r\f$ entries to both sides.
+	The margins (first and last \f$r\f$ entries of `data`) will be treated according to the \ref margins parameter.
+	In general, if both smoothing and cropping are to be applied, smooth first, crop second.
 	"""
 	def __init__(self,
 			radius: int = 5,
 			margins: str = "reduced",
 			*args, **kwargs):
 		"""
-		\todo Implement and document
+		Constructs a SlidingMean object.
+		\param radius \copybrief radius For more, see \ref radius.
+		\param margins \copybrief margins For more, see \ref margins.
+		\param *args Additional positional arguments, will be passed to the superconstructor.
+		\param **kwargs Additional keyword arguments will be passed to the superconstructor.
 		"""
 		super().__init__(*args, **kwargs)
 		## Setting, how the first and last `r` entries of `data` will be treated.
@@ -107,21 +139,18 @@ class SlidingMean(Filter):
 		## - `"reduced"`: (default) smoothing with reduced smoothing radius, such that the radius extends to the borders of `data`
 		## - `"flat"`:  the marginal entries get the same value applied, as the first/last fully smoothed entry.
 		self.margins = margins
-		## Smoothing radius for smoothing \ref strain and \ref strain_inst.
-		## Smoothes the record using a the mean over \f$2r + 1\f$ entries.
-		## For each entry, the sliding mean extends `r` entries to both sides.
-		## The margins (first and last `r` entries of `data`) will be treated according to the `margins` parameter.
-		## In general, if both smoothing and cropping are to be applied, smooth first, crop second.
+		## Smoothing radius for the data, number of entries of data to each side to be taken into account.
 		self.radius = radius
-	def run(self, data: np.array, radius: int = None, margins: str = None) -> np.array:
+	def run(self,
+			data: np.array,
+			radius: int = None,
+			margins: str = None,
+			) -> np.array:
 		"""
-		Smoothes the record using a the mean over \f$2r + 1\f$ entries.
-		For each entry, the sliding mean extends `r` entries to both sides.
-		The margins (first and last `r` entries of `data`) will be treated according to the `margins` parameter.
-		In general, if both smoothing and cropping are to be applied, smooth first, crop second.
+		\copydoc SlidingMean
 		\param data List of data to be smoothed.
-		\param r Smoothing radius.
-		\param margins \copydoc margins
+		\param radius \copybrief radius Defaults to \ref radius. For more, see \ref radius.
+		\param margins \copybrief margins Defaults to \ref margins. For more, see \ref margins.
 		"""
 		margins = margins if margins is not None else self.margins
 		r = radius if radius is not None else self.radius
