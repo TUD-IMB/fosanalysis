b405ab41c6c0f8aa06cbc0dee205086e258e75af
Bertram Richter <bertram.richter@tu-dresden.de>
2022-08-09 12:51:23 +0200

    Simplified shrink compensation.

--- fosanalysis/shrinking.py
+++ fosanalysis/shrinking.py
@@ -12,48 +12,36 @@ import fosutils
 
 class ShrinkCompensator():
 	def __init__(self,
-			strain_inst: np.array,
-			x_inst: np.array,
 			method: str = "mean_min",
 			*args, **kwargs):
 		"""
 		Constructs a ShrinkCompensator object.
-		\param strain_inst \copybrief strain_inst For more, see \ref strain_inst.
-		\param x_inst \copybrief x_inst For more, see \ref x_inst.
 		\param method \copybrief method For more, see \ref method.
 		\param args \copybrief args For more, see \ref args.
 		\param kwargs \copybrief kwargs For more, see \ref kwargs.
-		
-		\todo Apply sanitization to \ref strain_inst and \ref strain_inst.
-		\todo Take two \ref strainprofile.StrainProfile objects and return a \ref strainprofile.StrainProfile
 		"""
 		## Method, how to calculate the shrinkage calibration. Available options:
-		## - `"mean_min"`: (default) For all entries in local minima in `y_inst`, the difference to the same value in `y_inf` is measured.
+		## - `"mean_min"`: (default) For all entries in local minima in in the instantaneous strain `stain_inst`, the difference to the same value in `strain` is measured.
 		## 	Afterwards the mean over the differences is taken.
 		self.method = method
-		## Instantaneous strain, that appear right after the applying load to the structure. 
-		self.strain_inst = strain_inst
-		## Positional data according to \ref strain_inst.
-		self.x_inst = x_inst
 		## Positional arguments, will be passed to [`scipy.signal.find_peaks()`](https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html).
 		## By default empty.
 		self.args = args
 		## Keyword arguments, will be passed to [`scipy.signal.find_peaks()`](https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html).
 		## By default empty.
 		self.kwargs = kwargs
-	def run(self, x: np.array, strain: np.array) -> np.array:
+	def run(self, x: np.array, strain: np.array, strain_inst: np.array) -> np.array:
 			"""
 			The influence of concrete creep and shrinking is calculated.
+			All of the parameters are assumed to be in sync and sanitized.
 			\param x Positional data.
-			\param strain Strain data.
+			\param strain Strain data, belonging to `x`, that was measured with time delay after applying the load.
+			\param strain_inst Instantaneous strain belonging to `x`, that appear right after applying the load to the structure.
+			\return Returns an array of same length as the given arrays.
 			"""
-			peaks_min, properties = scipy.signal.find_peaks(-self.strain_inst, *self.args, **self.kwargs)
-			# Get x positions and y-values for instantanious deformation
-			y_min_inst = np.array([self.strain_inst[i] for i in peaks_min])
-			x_min_inst = np.array([self.x_inst[i] for i in peaks_min])
-			# Get x positions and y-values for deformation after a long time
-			x_min_inf_index = [fosutils.find_closest_value(x, min_pos)[0] for min_pos in x_min_inst]
-			y_min_inf = np.array([strain[i] for i in x_min_inf_index])
-			min_diff = y_min_inf - y_min_inst
-			shrink_calibration_values = np.full(len(self.strain), np.mean(min_diff))
+			peaks_min, properties = scipy.signal.find_peaks(-strain_inst, *self.args, **self.kwargs)
+			strain_min_inst = np.array([strain_inst[i] for i in peaks_min])
+			strain_min = np.array([strain[i] for i in peaks_min])
+			min_diff = np.mean(strain_min - strain_min_inst)
+			shrink_calibration_values = np.full(len(x), min_diff)
 			return shrink_calibration_values
