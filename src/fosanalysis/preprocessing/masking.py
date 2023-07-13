d72c0de7a35fff6da379ec0b518fa96484a3ac80
Bertram Richter <bertram.richter@tu-dresden.de>
2023-07-13 19:17:04 +0200

    Refine anomaly masking interface.

--- src/fosanalysis/preprocessing/masking.py
+++ src/fosanalysis/preprocessing/masking.py
@@ -19,7 +19,7 @@ from fosanalysis import fosutils
 class AnomalyMasker(fosutils.Base):
 	"""
 	Abstract class for anomaly identification.
-	In a signal, implausible data points are replaced by `NaN` values.
+	In a signal, implausible data points (strain reading anomalies (SRAs)) are replaced by `NaN` values, effectively marking them as dropouts.
 	"""
 	def __init__(self, *args, **kwargs):
 		"""
@@ -28,35 +28,37 @@ class AnomalyMasker(fosutils.Base):
 		\param **kwargs Additional keyword arguments will be passed to the superconstructor.
 		"""
 		super().__init__(*args, **kwargs)
-	def run(self, x: np.array,
-			y: np.array,
+	def run(self,
+			x_data: np.array,
+			y_data: np.array,
 			*args, **kwargs) -> np.array:
 		"""
 		Mask strain reading anomalies with `NaN`s.
 		The array, indicating which indizes are SRA is determined by \ref identify_SRAs().
 		The strain data is replaced by `NaN` for all entries in the returned array being `True`.
-		\param x Distance from the start of the sensor.
-		\param y Strain data according to `x`.
+		\param x_data Array of measuring point positions in accordance to `y_data`.
+		\param y_data Array of strain data in accordance to `x_data`.
 		\param *args Additional positional arguments, will be passed to \ref identify_SRAs().
 		\param **kwargs Additional keyword arguments will be passed to \ref identify_SRAs().
-		\return Returns the `y` array, with identified strain reading anomalies masked by `NaN`.
+		\return Returns the `y_data` array, with identified strain reading anomalies masked by `NaN`.
 		"""
-		masked = np.array(copy.deepcopy(y))
-		SRA_array = self.identify_SRAs(x=x, y=masked, *args, **kwargs)
+		masked = np.array(copy.deepcopy(y_data))
+		SRA_array = self.identify_SRAs(x_data=x_data, y_data=masked, *args, **kwargs)
 		masked[SRA_array] = float("nan")
 		return masked
 	@abstractmethod
-	def identify_SRAs(self, x: np.array,
-					y: np.array,
-					*args, **kwargs) -> np.array:
+	def identify_SRAs(self,
+			x_data: np.array,
+			y_data: np.array,
+			*args, **kwargs) -> np.array:
 		"""
 		Estimate, which entries are strain reading anomalies.
-		\param x Distance from the start of the sensor.
-		\param y Strain data according to `x`.
+		\param x_data Array of positional data.
+		\param y_data Array of data with functional data according to `x_data`.
 		\param *args Additional positional arguments, further specified in sub-classes.
 		\param **kwargs Additional keyword arguments, further specified in sub-classes.
-		\return Returns an array (same shape as `y`), where entries, which are identified as SRA, are set to `True`.
+		\return Returns an array (same shape as `y_data`), where entries, which are identified as SRA, are set to `True`.
 		"""
-		SRA_array = np.full_like(y, False, dtype=bool)
+		SRA_array = np.full_like(y_data, False, dtype=bool)
 		return SRA_array
 
