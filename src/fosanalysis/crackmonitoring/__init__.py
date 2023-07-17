4564515d865bc2d90e36fefd416712c2ac3a5e0f
Bertram Richter <bertram.richter@tu-dresden.de>
2023-07-17 08:36:46 +0200

    Add init files to make the new packages importable.

---
+++ src/fosanalysis/crackmonitoring/__init__.py
@@ -0,0 +1,12 @@
+
+"""
+Contains modules related to crack monitoring of concrete structures.
+
+\author Bertram Richter
+\date 2023
+"""
+
+from . import cracks
+from . import finding
+from . import separation
+from . import strainprofile
---
+++ src/fosanalysis/utils/__init__.py
@@ -0,0 +1,12 @@
+
+"""
+Contains utility modules with general pupose.
+
+\author Bertram Richter
+\date 2023
+"""
+
+from . import base
+from . import cropping
+from . import integration
+from . import misc
