3cf7519b46202a9c135f8f115b76025eb7eb002a
Bertram Richter <bertram.richter@tu-dresden.de>
2023-07-25 08:19:55 +0200

    Add utils.misc.sliding_window() generator function.

--- src/fosanalysis/utils/misc.py
+++ src/fosanalysis/utils/misc.py
@@ -61,3 +61,41 @@ def next_finite_neighbor(
 	if result_index is not None and recurse > 0:
 		result_index, result = next_finite_neighbor(array=array, index=result_index, to_left=to_left, recurse=recurse-1)
 	return result_index, result
+
+def sliding_window(data_array: np.array, radius) -> tuple:
+	"""
+	Generates a sliding window over an array.
+	This function returns a generator, hence, it should be use like:
+	```.py
+	for pixel, window in sliding_window(<array>, <radius>):
+		# do something
+		pass
+	```
+	In contrast to other function like
+	[`numpy.lib.stride_tricks.sliding_window_view()`](https://numpy.org/devdocs/reference/generated/numpy.lib.stride_tricks.sliding_window_view.html),
+	this window slides over each pixel, even in the margin areas of `data_array`.
+	So, for each entry in `data_array`, a view to the window surrounding it is yielded.
+	In the margins, the window contains fewer entries.
+	Thus, boundary effects are to be expected, when using this function.
+	\remark Note, that a views of the original array are yielded, not copies.
+		Changing a pixel's value in the window will change original array. 
+	\param data_array Array of data, over which the window should slide.
+	\param radius Inradius of the window, sets the window's widths.
+		Along an axis, the window has a width of \f$(2r+1)\f$.
+		It is expected to be an `int` or `iterable`. 
+		If `radius` is an integer, it is used for all axes.
+		The window will contain (up to) \f$(2r+1)^2\f$ entries.
+		To set different widths for axes, use an `iterable`, such as a `tuple`.
+		This `tuple`'s length has to match the dimension of `data_array`.
+	\return In each iteration, a tuple like `(pixel, window)` is yielded.
+	\retval pixel Index of the window's central pixel in `data_array`.
+	\retval window Sub-array view of the `data_array` centered around `pixel`. 
+	"""
+	data_array = np.array(data_array)
+	if isinstance(radius, int):
+		radius = (radius,)*data_array.ndim
+	iterator = np.nditer(data_array, flags=["multi_index"])
+	for pixel_value in iterator:
+		pixel = iterator.multi_index
+		slices = tuple(slice(max(p-r, 0), min(p+r+1, z+1)) for p, r, z in zip(pixel, radius, data_array.shape))
+		yield pixel, data_array[slices]
